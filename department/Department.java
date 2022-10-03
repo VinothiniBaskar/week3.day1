1e9805f61be986f8aff72d1f9f7ff64423b5f794
VinothiniBaskar <113995562+VinothiniBaskar@users.noreply.github.com>
2022-10-03 21:07:19 +0530

    Add files via upload

---
+++ department/Department.java
@@ -0,0 +1,14 @@
+package dep.department;
+
+import clg.college.College;
+
+//Parent  Class
+public class Department extends College {
+	
+	// Create a method
+	public void deptName() {
+		String deptName = "B.TECH IT";
+		System.out.println("The Department name is:" + deptName);
+	}
+
+}
