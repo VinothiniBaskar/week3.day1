e46d8ec7fc66a1bf30a2db8fa6a2462d799e4986
VinothiniBaskar <113995562+VinothiniBaskar@users.noreply.github.com>
2022-10-03 21:05:33 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,29 @@
+package clg.college;
+
+//Grant Parent class
+public class College {
+
+	// Variable Declaration
+	String clgName = "Indra Ganesan college of Engineering";
+	int clgCode = 1245;
+	int clgRank = 4;
+
+	// Create a method
+	public void collegeName() {
+		System.out.println("The college name is :" + clgName);
+
+	}
+
+	// Create a method
+	public void collegeCode() {
+		System.out.println("The college code is:" + clgCode);
+
+	}
+
+	// Create a method
+	public void collegeRank() {
+		System.out.println("The college Rank is :" + clgRank);
+
+	}
+
+}
