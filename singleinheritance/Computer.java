e3a9701730d52e2fe4c559ce672311a44e8bfa03
VinothiniBaskar <113995562+VinothiniBaskar@users.noreply.github.com>
2022-10-03 21:35:47 +0530

    Add files via upload

---
+++ singleinheritance/Computer.java
@@ -0,0 +1,14 @@
+package inherit.singleinheritance;
+
+public class Computer {
+
+	// Variable declaration
+	String model = "WDC WD2500LPCX-24C6HT0 ATA Device";
+
+	// Create a method
+	public void computerModel() {
+		System.out.println("The model of the computer is :" + model);
+
+	}
+
+}
---
+++ singleinheritance/Desktop.java
@@ -0,0 +1,22 @@
+package inherit.singleinheritance;
+
+public class Desktop extends Computer {
+
+	// Variable declaration
+	int size = 28;
+
+	// Create a method
+	public void getSize() {
+		System.out.println("The size of the Desktop is :" + size);
+	}
+
+	public static void main(String[] args) {
+
+		// Create an object for child class and access the parent class method
+		Desktop d = new Desktop();
+		d.computerModel();
+		d.getSize();
+
+	}
+
+}
